e33a080db88198337a8e9dd5c26f9ccca5f6a7bc
denniz-10 <Denilson@DESKTOP-VUKSJCI>
2018-06-02 18:17:33 -0300

    Patterns Criacionais - Singleton

---
+++ DesignPatterns/src/singleton/TesteIncremental.java
@@ -0,0 +1,36 @@
+package singleton;
+
+/**
+ * 
+ * @author Denilson | RA: 816122383
+ *
+ */
+class Incremental {
+	private static Incremental singletonInstance;
+	private static int count = 0;
+	private int numero;
+
+	private Incremental() {
+		numero = ++count;
+	}
+
+	synchronized static Incremental getInstance() {
+		if (singletonInstance == null)
+			singletonInstance = new Incremental();
+
+		return singletonInstance;
+	}
+
+	public String toString() {
+		return "Incremental: " + numero;
+	}
+}
+
+public class TesteIncremental {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			Incremental inc = Incremental.getInstance();
+			System.out.println(inc);
+		}
+	}
+}
